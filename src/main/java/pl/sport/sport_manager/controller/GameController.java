4a7aba4f49f4df75791d94ab700be631f455d604
Izabela Draga <izabdr@gmail.com>
2019-07-10 23:38:16 +0200

    log in working

--- src/main/java/pl/sport/sport_manager/controller/GameController.java
+++ src/main/java/pl/sport/sport_manager/controller/GameController.java
@@ -1,25 +1,21 @@
 package pl.sport.sport_manager.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import pl.sport.sport_manager.entity.GameTeam;
-import pl.sport.sport_manager.repository.GameTeamRepository;
+
 
 import java.util.List;
 
 @Controller
 public class GameController {
 
-    @Autowired
-    GameTeamRepository gameTeamRepository;
+//    @Autowired
+//    GameTeamRepository gameTeamRepository;
 
     @GetMapping("/createTeam")
-    public String stagesPageShow(Model model, long id) {
-        List<GameTeam> gameTeamList = gameTeamRepository.findAllByUserId(id);
+    public String stagesPageShow(Model model) {
+       // List<GameTeam> gameTeamList = gameTeamRepository.findAllByUserId(id);
         return "/game/teamCreate"; }
 }
 
