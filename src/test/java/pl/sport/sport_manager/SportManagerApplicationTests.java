1fbffb62f608201f88c3c85499b49d2399a22235
Izabela Draga <izabdr@gmail.com>
2019-07-10 23:39:56 +0200

    log in

---
+++ src/test/java/pl/sport/sport_manager/SportManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sport.sport_manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
